c52237322e371e9b5a072223d98248d88304ddb1
weishaoqiang <ysdxz207@qq.com>
2018-04-20 14:36:30 +0800

    :smile:

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,34 +1,9 @@
 <template>
   <div class="page has-navbar" v-nav="{ title: 'Home' }">
     <div class="page-content text-center">
-      <h2 class="padding" v-text="msg"></h2>
+      <div class="padding">
 
-      <router-link class="button button-assertive" to="/about">
-        <i class="ion-information-circled"></i> About
-      </router-link>
-
-      <div style="height: 40px;"></div>
-
-      <swiper ref="swiper" 
-        direction="horizontal" 
-        width="100%" 
-        height="150" 
-        pager-color="#ea5a49" 
-        pager-bg-color="#fff"
-        hide-pager="false"
-      >
-        <swiper-item style="background: #0a9dc7;">
-          <h3>Keep Hungry</h3>
-        </swiper-item>
-
-        <swiper-item style="background: #44cc00;">
-          <h3>Keep Foolish</h3>
-        </swiper-item>
-
-        <swiper-item style="background: #ffc900;">
-          <h3>..</h3>
-        </swiper-item>
-      </swiper>
+      </div>
     </div>
   </div>
 </template>
