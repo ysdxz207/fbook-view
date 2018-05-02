991b2bc22222732baf22ac242bd2863af18a2900
Moses <ysdxz207@qq.com>
2018-05-02 11:58:39 +0800

    :smile:

--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,57 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+    entry: './src/main.js',
     output: {
-      path: path.resolve(__dirname, './dist'),
-      publicPath: '/dist/',
-      filename: 'build.js',
-      chunkFilename: "[id].build.js"
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'build.js',
+        chunkFilename: "[id].build.js"
     },
     module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            scss: 'vue-style-loader!css-loader!sass-loader'
-          }
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        scss: 'vue-style-loader!css-loader!sass-loader'
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style-loader!css-loader!sass-loader'
+            },
+            {
+                test: /\.(png|jpg|gif|svg)|((eot|woff|ttf|svg)[\?]?.*)$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.vue'],
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
         }
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader'
-      },
-      {
-        test: /\.(png|jpg|gif|svg)|((eot|woff|ttf|svg)[\?]?.*)$/,
-        loader: 'url-loader',
-        query: {
-          limit: 10000,
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.vue'],
-    alias: {
-      'vue$': 'vue/dist/vue.common.js'
-    }
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: false
-  },
-  devtool: '#eval-source-map',
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: false
+    },
+    devtool: '#eval-source-map',
     externals: {
-        'vue':'Vue',
-        'vue-router':'VueRouter',
+        'vue': 'Vue',
+        'vue-router': 'VueRouter',
         'axios': 'axios',
         'vonic': 'vonic'
 
@@ -60,13 +60,21 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    })
-  ])
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new SshWebpackPlugin({
+            host: 'puyixiaowo.win',
+            port: '22',
+            username: 'root',
+            password: '//hupubao207',//or use privateKey login(privateKey: require('fs').readFileSync('/path/to/private/key')).
+            from: '/fbook-view',
+            to: '/app/fbook/view',//important: If the 'cover' of value is false,All files in this folder will be cleared before starting deployment.
+        })
+    ])
 }
